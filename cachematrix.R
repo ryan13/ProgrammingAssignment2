e9d002aa7e21db94eea6976c3987c0f7b1f29dd8
ryan13 <tri.sidoan@gmail.com>
2014-05-20 10:56:13 -0700

    additional part

--- cachematrix.R
+++ cachematrix.R
@@ -4,31 +4,38 @@
 ## its inverse. exposes setters and getters for both. invalidates
 ## cache on set.
 makeCacheMatrix <- function(x = matrix()) {
+  # initialize the value 
   tmp <- NULL
+  # set the value of the matrix
   set <- function(y) {
     x <<- y
     tmp <<- NULL
   }
+  #  get the value of the matrix
   get <- function() x
+  # set the inverse
   setinv <- function(inv) tmp <<- inv
+  # get the inverse
   getinv <- function() inverse
+  # return a list of all the above functions
   list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
+ 
 
-
-## computes the inverse of a matrix with caching.
-## expects a wrapped matrix as returned from makeCacheMatrix. first
-## checks cache in the object passed in for the presence of the result
-## of a previous run. if found returns that, otherwise computes the
-## result and then stores it in the cache as well as returning it to
-## the caller
+## Computes the inverse of a matrix with caching which returned by a function
+## makeCacheMatrix.
+##    = checks cache in the object passed in for the presence of the result
+## of a previous run. if found returns that, otherwise computes the result and 
+## stores it in the cache and returning it to the caller
 
 cacheSolve <- function(x) {
+  # check whether of not the inverse is cached
   tmp <- x$getinv()
   if(!is.null(tmp)) {
     message(" current cached data")
     return(tmp)
   }
+  # not cached, get the matrix into data
   data <- x$get()
   tmp <- solve(data)
   x$setinv(tmp)
